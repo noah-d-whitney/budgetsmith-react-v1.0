9076cc758c096b832eac48a2a7d1d217c952eba3
noah-d-whitney <me@noahwhitney.com>
2023-08-06 16:35:47 -0400

    Transaction Class and state

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import { Navbar } from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./components/HomePage";
 import { BudgetPage } from "./components/BudgetPage";
-import { Category } from "./dataClasses";
+import { Category, Transaction } from "./dataClasses";
 
 function App() {
+  // ANCHOR state
   const [firstName, setFirstName] = useState("Lexie");
   const [startingBalance, setStartingBalance] = useState(1000);
   const [categories, setCategories] = useState([
@@ -21,6 +22,17 @@ function App() {
     new Category("subscriptions", "expense", 80, "discretionary"),
     new Category("paycheck", "income", 2750),
     new Category("freelance", "income", 1000),
+  ]);
+  const [transactions, setTransactions] = useState([
+    new Transaction("rent", "expense", 750, "July Rent"),
+    new Transaction("car payment", "expense", 500),
+    new Transaction("gas", "expense", 40, "Filled tank"),
+    new Transaction("gas", "expense", 70, "Filled tank"),
+    new Transaction("groceries", "expense", 120, "Walmart Trip"),
+    new Transaction("groceries", "expense", 50, "Stuff for dinner"),
+    new Transaction("entertainment", "expense", 45, "Date night"),
+    new Transaction("paycheck", "income", 700),
+    new Transaction("freelance", "income", 500, "Web design contract"),
   ]);
 
   const balances = {
--- src/dataClasses.js
+++ src/dataClasses.js
@@ -9,3 +9,16 @@ export class Category {
     this.id = crypto.randomUUID();
   }
 }
+
+// ANCHOR Transaction{}
+export class Transaction {
+  constructor(category, type, amount, note, receipt) {
+    this.type = type;
+    this.amount = type === "expense" ? -amount : amount;
+    this.category = category;
+    this.note = note;
+    this.receipt = receipt;
+    this.date = new Date();
+    this.id = crypto.randomUUID();
+  }
+}
